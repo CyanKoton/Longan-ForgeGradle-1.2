86a03f1df3f5c291eaa5c750f6e7e1d5d92773bb
beanflame <68175497+beanflame@users.noreply.github.com>
2021-07-11 18:11:58 +0800

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -211,12 +211,12 @@ publishing {
         maven {
             name = "mavenCentral"
             url = if (version.toString().endsWith("SNAPSHOT"))
-                uri("https://oss.sonatype.org/content/repositories/snapshots")
-            else uri("https://oss.sonatype.org/service/local/staging/deploy/maven2/")
+                uri("https://s01.oss.sonatype.org/content/repositories/snapshots")
+            else uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
 
             credentials {
-                username = project.findProperty("com.anatawa12.sonatype.username")?.toString() ?: ""
-                password = project.findProperty("com.anatawa12.sonatype.passeord")?.toString() ?: ""
+                username = project.findProperty("ossUsername")?.toString() ?: ""
+                password = project.findProperty("ossPassword")?.toString() ?: ""
             }
         }
         */
