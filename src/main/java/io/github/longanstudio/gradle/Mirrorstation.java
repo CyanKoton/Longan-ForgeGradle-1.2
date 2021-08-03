aba2aa2f8cfbef76beff0a817ae25ffbb7a9c0c2
beanflame <68175497+beanflame@users.noreply.github.com>
2021-08-03 14:35:59 +0800

    Update Mirrorstation.java

--- src/main/java/io/github/longanstudio/gradle/Mirrorstation.java
+++ src/main/java/io/github/longanstudio/gradle/Mirrorstation.java
@@ -4,83 +4,93 @@ package io.github.longanstudio.gradle;
 import org.gradle.api.Project;
 
 
-// Mirrorstation
 
+// Mirrorstation
 
 public class Mirrorstation {
 
+    public static final String EXT_NAME_MIRRORSTATION = "mirrorstation";
+    
     // urls
-    private String MC_JSON_URL = "https://s3.amazonaws.com/Minecraft.Download/versions/{MC_VERSION}/{MC_VERSION}.json";
-    private String MC_JAR_URL = "https://s3.amazonaws.com/Minecraft.Download/versions/{MC_VERSION}/{MC_VERSION}.jar";
-    private String MC_SERVER_URL = "https://s3.amazonaws.com/Minecraft.Download/versions/{MC_VERSION}/minecraft_server.{MC_VERSION}.jar";
-    private String MCP_URL = "https://files.minecraftforge.net/fernflower-fix-1.0.zip";
-    private String ASSETS_URL = "https://resources.download.minecraft.net";
-    private String LIBRARY_URL = "https://libraries.minecraft.net/";
-    private String ASSETS_INDEX_URL = "https://s3.amazonaws.com/Minecraft.Download/indexes/{ASSET_INDEX}.json";
-
-
+    protected String mcJsonUrl = "https://s3.amazonaws.com/Minecraft.Download/versions/{MC_VERSION}/{MC_VERSION}.json";
+    protected String mcJarUrl = "https://s3.amazonaws.com/Minecraft.Download/versions/{MC_VERSION}/{MC_VERSION}.jar";
+    protected String mcServerUrl = "https://s3.amazonaws.com/Minecraft.Download/versions/{MC_VERSION}/minecraft_server.{MC_VERSION}.jar";
+    protected String mcpUrl = "https://files.minecraftforge.net/fernflower-fix-1.0.zip";
+    protected String assetsUrl = "https://resources.download.minecraft.net";
+    protected String libraryUrl = "https://libraries.minecraft.net/";
+    protected String assetsIndexUrl = "https://s3.amazonaws.com/Minecraft.Download/indexes/{ASSET_INDEX}.json";
     // maven
-    private String LONGAN_MAVEN_URL  = "https://longan.beanflame.cn/maven";
-    private String ALIYUN_MAVEN_URL  = "https://maven.aliyun.com/repository/public";
-    private String MAVEN_MAVEN_URL  = "https://repo1.maven.org/maven2";
-    private String FORGE_MAVEN_URL = "https://maven.minecraftforge.net";
+    protected String longanMavenUrl  = "https://longan.beanflame.cn/maven";
+    protected String aliyunMavenUrl  = "https://maven.aliyun.com/repository/public";
+    protected String mavenMavenUrl  = "https://repo1.maven.org/maven2";
+    protected String forgeMavenUrl = "https://maven.minecraftforge.net";
 
 
     // ---------------------------------------------------------------------------------------------------------------
 
 
-    public Mirrorstation() { }
-    public Mirrorstation(Project project) { System.out.println("project = " + project); }
+    /*
     public static Mirrorstation getInstance(Project project) {
-        return project.getExtensions().create("mirrorstation", Mirrorstation.class);
+        return project.getExtensions().create(EXT_NAME_MIRRORSTATION, Mirrorstation.class);
+    }
+    */
+
+    // public Mirrorstation(Project project) { System.out.println("project = " + project); }
+
+    public Mirrorstation(Project project) {
+        // System.out.println("project = " + project);
+
+        System.out.println("Mirrorstation - Version : 0.1.0");
     }
 
-    // MC_JSON_URL  ok
-    public String getMcJsonUrl() { return MC_JSON_URL; }
-    public void setMcJsonUrl(String mcJsonUrl) { this.MC_JSON_URL = mcJsonUrl; }
+
+    // mcJsonUrl  ok
+    public String getMcJsonUrl() { return mcJsonUrl; }
+    public void setMcJsonUrl(String mcJsonUrl) { this.mcJsonUrl = mcJsonUrl; }
 
     // MC_JAR_URL   ok
-    public String getMcJarUrl() { return MC_JAR_URL; }
-    public void setMcJarUrl(String mcJarUrl) { this.MC_JAR_URL = mcJarUrl; }
+    public String getMcJarUrl() { return mcJarUrl; }
+    public void setMcJarUrl(String mcJarUrl) { this.mcJarUrl = mcJarUrl; }
 
     // MC_SERVER_URL    ok
-    public String getMcServerUrl() { return MC_SERVER_URL; }
-    public void setMcServerUrl(String mcServerUrl) { this.MC_SERVER_URL = mcServerUrl; }
+    public String getMcServerUrl() { return mcServerUrl; }
+    public void setMcServerUrl(String mcServerUrl) { this.mcServerUrl = mcServerUrl; }
 
     // MCP_URL  ok
-    public String getMcpUrl() { return MCP_URL; }
-    public void setMcpUrl(String mcpUrl) { this.MCP_URL = mcpUrl; }
+    public String getMcpUrl() { return mcpUrl; }
+    public void setMcpUrl(String mcpUrl) { this.mcpUrl = mcpUrl; }
 
     // ASSETS_URL   ok
-    public String getAssetsUrl() { return ASSETS_URL; }
-    public void setAssetsUrl(String assetsUrl) { this.ASSETS_URL = assetsUrl; }
+    public String getAssetsUrl() { return assetsUrl; }
+    public void setAssetsUrl(String assetsUrl) { this.assetsUrl = assetsUrl; }
 
     // LIBRARY_URL  ok
-    public String getLibraryUrl() { return LIBRARY_URL; }
-    public void setLibraryUrl(String libraryUrl) { this.LIBRARY_URL = libraryUrl; }
+    public String getLibraryUrl() { return libraryUrl; }
+    public void setLibraryUrl(String libraryUrl) { this.libraryUrl = libraryUrl; }
 
     // ASSETS_INDEX_URL ok
-    public String getAssetsIndexUrl() { return ASSETS_INDEX_URL; }
-    public void setAssetsIndexUrl(String assetsIndexUrl) { this.ASSETS_INDEX_URL = assetsIndexUrl; }
+    public String getAssetsIndexUrl() { return assetsIndexUrl; }
+    public void setAssetsIndexUrl(String assetsIndexUrl) { this.assetsIndexUrl = assetsIndexUrl; }
 
 
     // -----------------------------------------------------------------------------------------------
 
     // LONGAN_MAVEN_URL ok
-    public String getLonganMavenUrl() { return LONGAN_MAVEN_URL; }
-    public void setLonganMavenUrl(String longanMavenUrl) { this.LONGAN_MAVEN_URL = longanMavenUrl; }
+    public String getLonganMavenUrl() { return longanMavenUrl; }
+    public void setLonganMavenUrl(String longanMavenUrl) { this.longanMavenUrl = longanMavenUrl; }
 
     // ALIYUN_MAVEN_URL ok
-    public String getAliyunMavenUrl() { return ALIYUN_MAVEN_URL; }
-    public void setAliyunMavenUrl(String aliyunMavenUrl) { this.ALIYUN_MAVEN_URL = aliyunMavenUrl; }
+    public String getAliyunMavenUrl() { return aliyunMavenUrl; }
+    public void setAliyunMavenUrl(String aliyunMavenUrl) { this.aliyunMavenUrl = aliyunMavenUrl; }
 
     // MAVEN_MAVEN_URL  ok
-    public String getMavenMavenUrl() { return MAVEN_MAVEN_URL; }
-    public void setMavenMavenUrl(String mavenMavenUrl) { this.MAVEN_MAVEN_URL = mavenMavenUrl; }
+    public String getMavenMavenUrl() { return mavenMavenUrl; }
+    public void setMavenMavenUrl(String mavenMavenUrl) { this.mavenMavenUrl = mavenMavenUrl; }
 
     // FORGE_MAVEN_URL  ok
-    public String getForgeMavenUrl() { return FORGE_MAVEN_URL; }
-    public void setForgeMavenUrl(String forgeMavenUrl) { this.FORGE_MAVEN_URL = forgeMavenUrl; }
+    public String getForgeMavenUrl() { return forgeMavenUrl; }
+    public void setForgeMavenUrl(String forgeMavenUrl) { this.forgeMavenUrl = forgeMavenUrl; }
 
 
 }
+
